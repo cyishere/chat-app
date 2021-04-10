c3d704e14d37ce984e7ad0097d0c2cb78ab233b8
cyishere <cyishere@gmail.com>
2021-04-10 19:53:21 +0800

    add sendMessage mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -130,5 +130,38 @@ module.exports = {
         throw new UserInputError("User Not Found", err);
       }
     },
+    sendMessage: async (_, { to, content }, { user, prisma }) => {
+      try {
+        if (!user) throw new AuthenticationError("You need to login.");
+
+        const recipient = await prisma.user.findUnique({
+          where: {
+            username: to,
+          },
+        });
+
+        if (!recipient) {
+          throw new UserInputError("User not found.");
+        } else if (recipient.username === user.username) {
+          throw new UserInputError("You can't message yourself.");
+        }
+
+        if (content.trim() === "")
+          throw new UserInputError("Message is empty.");
+
+        const message = await prisma.message.create({
+          data: {
+            content,
+            from: user.username,
+            to,
+          },
+        });
+
+        return message;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
   },
 };
